6946813f854e8f56143f9cc8a29ac4a69a18acda
Emanuel Torres <emanuelema562@gmail.com>
2025-11-11 22:32:14 -0300

    11/11/2025

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-console.log("Hola mundooo!");
-
-//formas de declarar variables
-//const constante = "hola";
-//let variable = 5;
+//Airtable
+const airtableToken = "patrlX6gGjWJ4rTXi.78e0dc6576eab5f825e32ba84a3880e816e7846934cca7ca082d5248d495ce87";
+ const baseId = "appn9RLoP0VhtfoET";
+ const tableName = "Products";
+ const airtableUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 
 //data
 
-const listProductos = [
-    { id: 1, nombre: "Camara de red domo fija ColorVU 4K HIKVISION", modelo: "DS-2CD2187G2-L(SU)", precio: 100, img: '../img/camara fija domo Hikvision 4K.png' },
-    { id: 2, nombre: "Cámara bala TiOC HDCVI inteligente con doble luz 4K", modelo: "HAC-ME1809TH-A-PV", precio: 200, img: '../img/Cámara bala TiOC HDCVI inteligente con doble luz 4K.png' },
-    { id: 3, nombre: "Dahua Picoo Dual 10mp P5d-5f-pv Camara Wifi Lente 5mp+5mp Color Blanco", modelo: "DH-P5D-5F-PV", precio: 300, img: '../img/DH-P5D-5F-PV - CAMARA Dahua.png' },
-    { id: 4, nombre: "Cámara Seguridad Hikvision Turbo Hd Tvi 1080p 2mp Domo Color Blanco", modelo: "Ds-2ce76d0t-Exipf", precio: 400, img: '../img/Cám Hikvision Turbo Hd Tvi 1080p 2mp Domo' },
-    { id: 5, nombre: "Camara Bullet Dahua 5mp Full Hd Exterior Seguridad Cctv Ir 30mts", modelo: "HAC-HFW1500CP-0280B-S2", precio: 500, img: '../img/camara Bullet Dahua 5mp.png' },
-];
-
 //elementos del DOM
-const productsDom = document.querySelector('.muestra-product');//elemento padre
-
+const productsDom = document.querySelector('.section-product');//elemento padre
+const inputBusqueda = document.getElementById("busqueda");
 
 //funciones
 function creacionProducto(product){
@@ -48,6 +40,10 @@ function creacionProducto(product){
     return newProduct;//devolvemos el nuevo elemento creado al Dom
 }
 
+function filtroProductos(text){
+    const filtrado = listProductos.filter( (p => p.nombre.toLowerCase().includes(text.toLowerCase())))
+    return filtrado;
+}
 
 function muestraProductos(productos){
     productsDom.innerHTML = "";//limpiamos el contenido actual
@@ -59,6 +55,65 @@ function muestraProductos(productos){
 
 
 //EVENTOS
+inputBusqueda.addEventListener('keyup', (event) => {
+    const texto = event.target.value;//obtenemos el texto del input
+    const productosFiltrados = filtroProductos(texto);//obtenemos los productos filtrados
+    muestraProductos(productosFiltrados);//mostramos los productos filtrados
+})
 
 //Inicia programa
  muestraProductos(listProductos);
+
+
+
+ async function getProductsFromAirtable() {
+    try {
+        const response = await fetch(airtableUrl, {
+            headers: {
+                'Authorization': `Bearer ${airtableToken}`,
+                'content-type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log('Products from airtable:', data);
+    }
+    catch (error) {
+        console.error('Error fetching products from Airtable:', error);
+    }
+}
+
+getProductsFromAirtable();
+
+/*async function editAirtableProduct(product) {
+    try {
+        const response = await fetch(`${airtableUrl}/recqGCxjt90Sadm7s`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${airtableToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fields: {
+                    name: product.nombre,
+                    model: product.modelo,
+                    price: product.precio,
+                    image: product.img
+                }
+            })
+        });
+        const data = await response.json();
+        console.log('Product updated in Airtable:', data);
+    } catch (error) {
+        console.error('Error updating product in Airtable:', error);
+    }
+};
+
+//editAirtableProduct(listProductos[0]);
+editAirtableProduct(
+    {
+        nombre: "Camara de red domo fija ColorVU 4K HIKVISION EDITADA",
+        modelo: "DS-2CD2187G2-L(SU) EDITADA",
+        precio: 150,
+        img: '../img/camara fija domo Hikvision 4K.png'
+    }
+);*/
