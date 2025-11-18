8813c58e3153ff768dfe09b7ce6c471f3773d04e
Emanuel Torres <emanuelema562@gmail.com>
2025-11-18 13:15:40 -0300

    18/11/2025V2

--- JS/detalleProducto.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('#btnComprar');//seleccionamos el boton
-
-button.addEventListener('click', () => { //cuando se haga click en el boton
-    alert('Haz hecho click en el boton');
-});
